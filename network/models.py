f3841937c5386e65c101d29421651f00a2c8f526
root <simotasi@yahoo.gr>
2022-11-05 15:55:11 +0200

    Create device class and changes on urls.py, admin.py in order to create objects in admin GUI

--- network/models.py
+++ network/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+#Create your models here. We make classes to create models that we can create afterwards in the GUI
+class Device(models.Model):
+    name = models.CharField(max_length=100)
+    host = models.CharField(max_length=70)
+    username = models.CharField(max_length=100)
+    password = models.CharField(max_length=100, blank=True)
+    device_type = models.CharField(
+        max_length=30, choices=(("router", "Router"), ("switch", "Switch"), ("firewall", "Firewall")), blank=True
+    )
+    platform = models.CharField(
+        max_length=30, choices=(("cisco_ios", "Cisco IOS"), ("cisco_iosxe", "Cisco IOS XE")), blank=True
+    )
 
-# Create your models here.
+
--- network/views.py
+++ network/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpRequest, JsonResponse
 
+def index(request: HttpRequest) -> HttpResponse:
+    return HttpResponse("<h1>Hello stream</h1>")
 # Create your views here.
