a33d3d700fab11fedc08875a9d2d519c30c60994
Iasimo92 <simotasi@yahoo.gr>
2022-11-30 17:05:07 +0200

    HTML Introduction in the GUI

--- network/views.py
+++ network/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpRequest, JsonResponse
+from .models import Device
 
 def index(request: HttpRequest) -> HttpResponse:
-    return HttpResponse("<h1>Hello stream</h1>")
+    devices = Device.objects.all()
+    context = {
+        'title': 'Hello user of this network controller',
+        'devices': devices
+    }
+    return render(request, 'base.html', context)
+
+
+def get_devices(request: HttpRequest)->HttpResponse:
+    pass
+ 
 # Create your views here.
